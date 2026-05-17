ebe547e5127ba56d9e45a7934add2208803f4848
valval73 <romence1@gmail.com>
2026-05-17 08:31:39 +0200

    Fix inject: use rfind to find last </body>

--- inject_patch.py
+++ inject_patch.py
@@ -2,9 +2,15 @@ with open('index.html', 'r', encoding='utf-8') as f:
     html = f.read()
 tag = '<script src="live_patch.js"></script>'
 if tag not in html:
-    html = html.replace('</body>', tag + chr(10) + '</body>', 1)
-    with open('index.html', 'w', encoding='utf-8') as f:
-        f.write(html)
-    print('Injected live_patch.js into index.html')
+    # Inserer juste avant </body> mais APRES le dernier </script>
+    # Utiliser rfind pour trouver la DERNIERE occurrence de </body>
+    idx = html.rfind('</body>')
+    if idx != -1:
+        html = html[:idx] + tag + chr(10) + html[idx:]
+        with open('index.html', 'w', encoding='utf-8') as f:
+            f.write(html)
+        print('Injected live_patch.js into index.html')
+    else:
+        print('ERROR: </body> not found')
 else:
     print('live_patch.js already present')
