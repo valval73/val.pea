b18eaead114cd24322b17149a9999e2defb40b37
valval73 <romence1@gmail.com>
2026-05-17 08:34:40 +0200

    Fix inject: remove+reinsert at last </body>

--- inject_patch.py
+++ inject_patch.py
@@ -1,16 +1,14 @@
 with open('index.html', 'r', encoding='utf-8') as f:
     html = f.read()
 tag = '<script src="live_patch.js"></script>'
-if tag not in html:
-    # Inserer juste avant </body> mais APRES le dernier </script>
-    # Utiliser rfind pour trouver la DERNIERE occurrence de </body>
-    idx = html.rfind('</body>')
-    if idx != -1:
-        html = html[:idx] + tag + chr(10) + html[idx:]
-        with open('index.html', 'w', encoding='utf-8') as f:
-            f.write(html)
-        print('Injected live_patch.js into index.html')
-    else:
-        print('ERROR: </body> not found')
+# 1. Supprimer toute occurrence existante (peu importe où elle est)
+html_clean = html.replace(tag + chr(10), '').replace(tag, '')
+# 2. Injecter juste avant le DERNIER </body>
+idx = html_clean.rfind('</body>')
+if idx == -1:
+    print('ERROR: </body> not found')
 else:
-    print('live_patch.js already present')
+    html_new = html_clean[:idx] + tag + chr(10) + html_clean[idx:]
+    with open('index.html', 'w', encoding='utf-8') as f:
+        f.write(html_new)
+    print('OK: live_patch.js injected before last </body>')
